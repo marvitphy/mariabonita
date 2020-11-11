425bf89ed6c6667f137842e628ca66e676abcae0
Trincheira - Comunicação Visual <48634870+marvitphy@users.noreply.github.com>
2020-11-11 16:30:42 -0300

    alteracao

--- index.js
+++ index.js
@@ -19,10 +19,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.get('/', function(req, res) {
-    res.render('Home', {
-        key: Publishable_Key,
-        teste: 'Oi'
-    })
+    res.render('Home')
 })
 app.get('/Teste2', function(req, res) {
     res.render('Teste2')
